72eae1a95c12d6102cfc44726b97215f4ed6851a
leozecs <leonrt869@exemplo.com>
2025-03-29 14:32:57 -0300

    Primeiro commit

---
+++ src/test/java/study/dio/Decola_Tech_2025/DecolaTech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.dio.Decola_Tech_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
